37cf73f3bcaeeb7d08a402a966a8adeec1cc0ef7
Dongjun Xie <joycexie0615@gmail.com>
2024-07-12 20:26:27 -0700

    first commit

--- App.js
+++ App.js
@@ -1,4 +1,18 @@
 import express from 'express';
-const express = require('express') 
-const app = express() 
-app.listen(4000)
+import Hello from './Hello.js';
+import Lab5 from './Lab5/index.js';
+import CourseRoutes from './Kanbas/Courses/routes.js';
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import cors from "cors"
+
+
+const app = express()
+app.use(cors());
+app.use(express.json()); // Do all work after this line
+Hello(app)
+CourseRoutes(app)
+ModuleRoutes(app)
+AssignmentRoutes(app)
+Lab5(app)
+app.listen(process.env.PORT || 4000)
